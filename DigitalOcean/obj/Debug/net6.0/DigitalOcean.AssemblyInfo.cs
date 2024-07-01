cff7da9f11d7d8324638b82e7e5575f5ba0b2b26
o.reznik <o.reznik@belitsoft.com>
2024-07-01 09:31:18 +0300

    Removed DockerFile

--- DigitalOcean/obj/Debug/net6.0/DigitalOcean.AssemblyInfo.cs
+++ DigitalOcean/obj/Debug/net6.0/DigitalOcean.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalOcean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75636465e4d82be30e0029f25b56559d908b19e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b1f337cb58ee7ffdd137a945b2eea0c5e2f61b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalOcean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalOcean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
